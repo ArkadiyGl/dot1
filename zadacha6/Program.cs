f123ec69f5a4a2a666bdf834752b37841f6b4da2
ArkadiyGl <prezidentadutor@gmail.com>
2022-02-10 01:29:07 +0300

    4 commit- 6zadacha запомнить $ и !

--- zadacha6/Program.cs
+++ zadacha6/Program.cs
@@ -1,2 +1,15 @@
 ﻿// Задача 6: Выяснить является ли число чётным
-Console.WriteLine("Hello, World!");
+// если делится на 2 - то тогда да. 
+
+
+Console.Clear();
+System.Console.WriteLine();
+System.Console.Write("Введите число:    ");
+string writeNumber = Console.ReadLine();
+int number = Convert.ToInt32(writeNumber);
+string result = $"Число {number} является чётным";
+
+if (number % 2 != 0) result = result.Replace("является", "не является");
+
+System.Console.WriteLine(result);
+System.Console.WriteLine();
