dbd74f9557f1d7060285d4c29e994de549cb29a9
ArkadiyGl <prezidentadutor@gmail.com>
2022-02-20 23:05:27 +0300

    dz po 4 уроку

---
+++ zadacha26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+
+System.Console.WriteLine();
+
+int number = 5;
+int exponent = 25;
+int count = 1;
+int result = number;
+
+while (count < exponent)
+{
+    result = result * number;
+    count++;
+}
+
+System.Console.WriteLine($"{number} ^ {exponent} = {result}");
+System.Console.WriteLine();
